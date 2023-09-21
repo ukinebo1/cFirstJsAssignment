dfc2a44e8700bdfbcb825efeccfda932971a4e21
ukinebo1 <112960629+ukinebo1@users.noreply.github.com>
2023-09-21 03:30:34 +0100

    JavaScript assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+let myName =prompt("Enter your name", '');
+let myWeight =prompt("Enter your weight");
+
+function myBodyWeight(name, weight){
+    
+     if(isNaN(weight)){
+         alert('Invalid input');
+     }
+    let newWeight =Number(weight * 2.20462);
+    //console.log(newWeight)
+    
+    let result = `My Name is ${name} and I weigh ${newWeight}`;
+    alert(result);
+
+}myBodyWeight(myName, myWeight)
+
+
+
+
+
+
